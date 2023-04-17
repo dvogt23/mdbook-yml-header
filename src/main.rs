02eda42ce66c2a2b8900036586892ad07eaf7bf0
Jan Loewe <kontakt@jloewe.net>
2023-04-17 12:25:22 +0200

    fix(regex): match windows line endings aswell

--- src/main.rs
+++ src/main.rs
@@ -64,7 +64,7 @@ fn handle_supports(pre: &dyn Preprocessor, sub_args: &ArgMatches) -> ! {
 }
 
 lazy_static! {
-    static ref HEADER_RE: Regex = Regex::new(r"(?P<header>---\n((.+\n)+)---)").unwrap();
+    static ref HEADER_RE: Regex = Regex::new(r"(?P<header>---\r?\n((.+\r?\n)+)---)").unwrap();
 }
 
 /// A pre-processor that removes header between ---
