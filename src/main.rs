4a4ab3950f583941e0f207b61493aa5d056f6cdb
dima <9352647+dvogt23@users.noreply.github.com>
2025-11-29 13:27:38 +0100

    support mdbook 0.5.0 (#86)
    Co-authored-by: pinage404 <pinage404@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use clap::{App, Arg, ArgMatches, SubCommand};
 use lazy_static::lazy_static;
-use mdbook::{
-    book::Book,
+use mdbook_preprocessor::{
+    book::{Book, BookItem},
     errors::Error,
-    preprocess::{CmdPreprocessor, Preprocessor, PreprocessorContext},
+    parse_input, Preprocessor, PreprocessorContext,
 };
 use regex::Regex;
 use std::{io, process};
@@ -31,16 +31,16 @@ fn main() {
 }
 
 fn handle_preprocessing(pre: &dyn Preprocessor) -> Result<(), Error> {
-    let (ctx, book) = CmdPreprocessor::parse_input(io::stdin())?;
+    let (ctx, book) = parse_input(io::stdin())?;
 
-    if ctx.mdbook_version != mdbook::MDBOOK_VERSION {
+    if ctx.mdbook_version != mdbook_preprocessor::MDBOOK_VERSION {
         // We should probably use the `semver` crate to check compatibility
         // here...
         eprintln!(
             "Warning: The {} plugin was built against version {} of mdbook, \
              but we're being called from version {}",
             pre.name(),
-            mdbook::MDBOOK_VERSION,
+            mdbook_preprocessor::MDBOOK_VERSION,
             ctx.mdbook_version
         );
     }
@@ -56,7 +56,7 @@ fn handle_supports(pre: &dyn Preprocessor, sub_args: &ArgMatches) -> ! {
     let supported = pre.supports_renderer(&renderer);
 
     // Signal whether the renderer is supported by exiting with 1 or 0.
-    if supported {
+    if supported.is_ok_and(|supported_value| supported_value) {
         process::exit(0);
     } else {
         process::exit(1);
@@ -78,17 +78,15 @@ impl Preprocessor for YMLHeader {
 
     fn run(&self, _ctx: &PreprocessorContext, mut book: Book) -> Result<Book, Error> {
         book.for_each_mut(|item| {
-            if let mdbook::book::BookItem::Chapter(chap) = item {
-                chap.content = HEADER_RE
-                    .replace(&chap.content, "")
-                    .to_string();
+            if let BookItem::Chapter(chap) = item {
+                chap.content = HEADER_RE.replace(&chap.content, "").to_string();
             }
         });
 
         Ok(book)
     }
 
-    fn supports_renderer(&self, renderer: &str) -> bool {
-        renderer != "not-supported"
+    fn supports_renderer(&self, renderer: &str) -> Result<bool, Error> {
+        Ok(renderer != "not-supported")
     }
 }
